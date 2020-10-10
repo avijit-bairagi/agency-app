cf3580fdd4b3014a0bba09a341e6adb1631c1314
avijit-bkash <avijit.i2gether@bkash.com>
2020-10-11 03:05:11 +0600

    date format and pagination added

--- src/main/java/com/avijit/agencyapp/common/Constants.java
+++ src/main/java/com/avijit/agencyapp/common/Constants.java
@@ -8,4 +8,7 @@ public class Constants {
     public static final List<String> PRIVACY_TYPES = Arrays.asList("Public", "Private");
     public static final String PUBLIC = "Public";
     public static final String PRIVATE = "Private";
+    public static final int PAGE_SIZE = 3;
+
+    public static final String DATE_FORMAT_yyyy_MM_dd_hh_mm_a = "yyyy-MM-dd hh:mm a";
 }
