c16ad6f3e4d3a3ca5e21f655ba4c5e540b3a0ca7
avijit-bkash <avijit.i2gether@bkash.com>
2020-10-10 21:19:43 +0600

    constant file added

---
+++ src/main/java/com/avijit/agencyapp/common/Constants.java
@@ -0,0 +1,9 @@
+package com.avijit.agencyapp.common;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+
+    public static final List<String> PRIVACY_TYPES = Arrays.asList("Public", "Private");
+}
