0e5d4fb547e072d486d4118e75a9af3501831c0d
avijit-bkash <avijit.i2gether@bkash.com>
2020-10-10 18:20:27 +0600

    audit aware config added

---
+++ src/main/java/com/avijit/agencyapp/config/AuditorAwareImpl.java
@@ -0,0 +1,22 @@
+package com.avijit.agencyapp.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetails = (UserDetails) auth.getPrincipal();
+            return Optional.of(userDetails.getUsername());
+        }
+        return Optional.of("");
+    }
+}
