8d936678cdefffd0a65df06170f6527deb318d14
avijit-bkash <avijit.i2gether@bkash.com>
2020-10-10 22:48:26 +0600

    private and public string added

--- src/main/java/com/avijit/agencyapp/common/Constants.java
+++ src/main/java/com/avijit/agencyapp/common/Constants.java
@@ -6,4 +6,6 @@ import java.util.List;
 public class Constants {
 
     public static final List<String> PRIVACY_TYPES = Arrays.asList("Public", "Private");
+    public static final String PUBLIC = "Public";
+    public static final String PRIVATE = "Private";
 }
