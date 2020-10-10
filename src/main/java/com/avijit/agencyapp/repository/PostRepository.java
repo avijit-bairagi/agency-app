5a18f24683c34f5ffc2bb06fecb1cf6653423c2d
avijit-bkash <avijit.i2gether@bkash.com>
2020-10-11 03:06:54 +0600

    added native method to repository

--- src/main/java/com/avijit/agencyapp/repository/PostRepository.java
+++ src/main/java/com/avijit/agencyapp/repository/PostRepository.java
@@ -1,9 +1,18 @@
 package com.avijit.agencyapp.repository;
 
 import com.avijit.agencyapp.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+    Page<PostEntity> findByPrivacyType(Pageable pageable, String privacyType);
+
+    @Query(value = "SELECT * FROM posts p " +
+            "WHERE p.user_id = ?1 ORDER BY p.updated_at desc", nativeQuery = true)
+    Page<PostEntity> findAllCurrentUserPost(Pageable pageable, Long id);
 }
