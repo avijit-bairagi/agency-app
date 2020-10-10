4bc5bbce2cae5c21a9386d942421d7b43fe430de
avijit-bkash <avijit.i2gether@bkash.com>
2020-10-10 21:21:03 +0600

    added getter, setter annotation

--- src/main/java/com/avijit/agencyapp/entity/LocationEntity.java
+++ src/main/java/com/avijit/agencyapp/entity/LocationEntity.java
@@ -1,9 +1,14 @@
 package com.avijit.agencyapp.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity(name = "locations")
+@Getter
+@Setter
 public class LocationEntity {
 
     @Id
